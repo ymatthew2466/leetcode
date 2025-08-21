7f7bc6b2b8b8f5156ac9e1572fff4f36340661c7
ymatthew2466 <ymatthew2466@gmail.com>
2025-08-21 19:57:03 -0400

    3110

--- arrays-hashing/3110.py
+++ arrays-hashing/3110.py
@@ -1,1 +1,17 @@
 # 3110. Score of a String
+
+class Solution:
+    def scoreOfString(self, s: str) -> int:
+        '''
+        ord(char) to get ASCII
+
+        iterate thru and maintain count
+        '''
+        
+        count = 0
+        for i in range(len(s) -1):
+            curr = ord(s[i])
+            nxt = ord(s[i+1])
+            diff = abs(curr - nxt)
+            count += diff
+        return count
